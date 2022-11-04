2169c15a4ed2cb63a0c486e444a1579f32b3539e
johnsonsudre <johnsonsudre@gmail.com>
2022-11-04 07:38:29 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+import * as THREE from "./three.module.js";
+import {OrbitControls} from "./OrbitControls.js";
+
+class App {
+  constructor() {
+    
+  }
+
+  resize() {}
+
+  render() {}
+}
+
+export { App };
