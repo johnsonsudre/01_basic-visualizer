4625c104e930f4b473d7308d37a29a563e364c1b
johnsonsudre <johnsonsudre@gmail.com>
2022-11-04 08:29:27 -0300

    scene lighting

--- app.js
+++ app.js
@@ -1,25 +1,41 @@
 import * as THREE from "./three.module.js";
-import {OrbitControls} from "./OrbitControls.js";
+import { OrbitControls } from "./OrbitControls.js";
+
+function newBox(params) {
+  const geometry = new THREE.BoxBufferGeometry();
+  const material = new THREE.MeshStandardMaterial({ color: 0x44ff44 });
+  return new THREE.Mesh(geometry, material);
+}
 
 class App {
   constructor() {
     const container = document.createElement("div");
     document.body.appendChild(container);
-    
-    const aspectRatio = window.innerWidth/window.innerHeight;
+
+    const aspectRatio = window.innerWidth / window.innerHeight;
     this.camera = new THREE.PerspectiveCamera(60, aspectRatio, 0.1, 1000);
-    this.camera.position.set(0,1.65,5);
-    
+    this.camera.position.set(0, 0, 5);
+
     this.scene = new THREE.Scene();
     this.scene.add(this.camera);
-    this.scene.background = new THREE.Color(0xBBBBBB);
-    
-    this.renderer = new THREE.WebGLRenderer({antialias: true});
+    this.scene.background = new THREE.Color(0xbbbbbb);
+
+    this.ambientLight = new THREE.HemisphereLight(0xddeeff, 0x333300, 1);
+    this.scene.add(this.ambientLight);
+
+    this.light = new THREE.DirectionalLight();
+    this.light.position.set(5, 3, 5);
+    this.scene.add(this.light);
+
+    this.renderer = new THREE.WebGLRenderer({ antialias: true });
     this.renderer.setPixelRatio(window.devicePixelRatio);
     this.renderer.setSize(window.innerWidth, window.innerHeight);
     container.appendChild(this.renderer.domElement);
 
     this.renderer.setAnimationLoop(this.render.bind(this));
+
+    this.box = newBox();
+    this.scene.add(this.box);
 
     window.addEventListener("resize", this.resize.bind(this));
   }
@@ -27,6 +43,7 @@ class App {
   resize() {}
 
   render() {
+    this.box.rotation.y += 0.0175;
     this.renderer.render(this.scene, this.camera);
   }
 }
