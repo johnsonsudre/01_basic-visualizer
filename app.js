2420b3b41b89ba4bcb2eec5973eddf96b1344129
johnsonsudre <johnsonsudre@gmail.com>
2022-11-04 17:13:56 -0300

    add shadow

--- app.js
+++ app.js
@@ -3,12 +3,14 @@
 import * as THREE from "./modules/three.module.js";
 import { OrbitControls } from "./modules/OrbitControls.js";
 import { GLTFLoader } from "./modules/GLTFLoader.js";
+import { RGBELoader } from "./modules/RGBELoader.js";
 import { LoadingBar } from "./libs/LoadingBar.js";
 
 function newMesh(params) {
   const geometry = new THREE.CylinderGeometry(0.3, 0.3, 0.025, 64);
   const material = new THREE.MeshStandardMaterial({ color: 0xcccccc });
   const mesh = new THREE.Mesh(geometry, material);
+  mesh.receiveShadow = true;
   return mesh;
 }
 
@@ -16,11 +18,11 @@ function getAspectRatio() {
   return window.innerWidth / window.innerHeight;
 }
 
-class OrbitController{
-  constructor(scope){
+class OrbitController {
+  constructor(scope) {
     const control = new OrbitControls(scope.camera, scope.renderer.domElement);
     control.enablePan = false;
-    control.target = new THREE.Vector3(0,0.1,0);
+    control.target = new THREE.Vector3(0, 0.1, 0);
     control.update();
     return control;
   }
@@ -32,32 +34,44 @@ class App {
     document.body.appendChild(container);
 
     this.camera = new THREE.PerspectiveCamera(60, getAspectRatio(), 0.02, 10);
-    this.camera.position.set(0, 0.2, 0.4);
+    this.camera.position.set(0, 0.2, 0.65);
 
     this.scene = new THREE.Scene();
     this.scene.add(this.camera);
     this.scene.background = new THREE.Color(0xbbbbbb);
 
-    this.ambientLight = new THREE.HemisphereLight(0xddeeff, 0x333300, 2);
+    this.ambientLight = new THREE.HemisphereLight(0xddeeff, 0x333300, 0.5);
     this.scene.add(this.ambientLight);
 
-    this.light = new THREE.DirectionalLight();
-    this.light.position.set(5, 3, 5);
+    this.light = new THREE.DirectionalLight(0xffffff, 1);
+    this.light.position.set(0.3, 0.6, 0.3);
+    this.light.target.position.set(0, 0, 0);
+    this.light.castShadow = true;
+    this.light.shadowCameraVisible = true; // only for debugging
+    this.light.shadow.mapSize.width = 512*4; // default
+    this.light.shadow.mapSize.height = 512*4; // default
+
     this.scene.add(this.light);
+    this.scene.add(this.light.target);
 
     this.renderer = new THREE.WebGLRenderer({ antialias: true });
     this.renderer.setPixelRatio(window.devicePixelRatio);
     this.renderer.setSize(window.innerWidth, window.innerHeight);
+    this.renderer.outputEncoding = THREE.sRGBEncoding;
+    this.renderer.shadowMap.enabled = true;
+    this.renderer.shadowMap.type = THREE.PCFSoftShadowMap; // default THREE.PCFShadowMap
+
     container.appendChild(this.renderer.domElement);
+    this.setEnvironment();
 
     this.renderer.setAnimationLoop(this.render.bind(this));
 
     this.box = newMesh();
     this.scene.add(this.box);
-    this.asset = new THREE.Object3D;
-    
+    this.asset = new THREE.Object3D();
+
     this.loadingBar = new LoadingBar();
-    
+
     this.loadGLTF();
 
     this.control = new OrbitController(this);
@@ -65,18 +79,42 @@ class App {
     window.addEventListener("resize", this.resize.bind(this));
   }
 
+  setEnvironment() {
+    const loader = new RGBELoader().setDataType(THREE.HalfFloatType);
+    const pmremGenerator = new THREE.PMREMGenerator(this.renderer);
+    pmremGenerator.compileEquirectangularShader();
+    const self = this;
+    loader.load(
+      "./assets/textures/sandsloot_1k.hdr",
+      (texture) => {
+        const envMap = pmremGenerator.fromEquirectangular(texture);
+        self.scene.environment = envMap;
+        pmremGenerator.dispose();
+      },
+      (xhr) => {},
+      (err) => {
+        console.err(err);
+      }
+    );
+  }
+
   loadGLTF() {
-    const loader = new GLTFLoader().setPath("./assets/");
+    const loader = new GLTFLoader().setPath("./assets/models/");
     loader.load(
       "african_figurine_game_ready__2k_pbr.glb",
       (gltf) => {
-        gltf.scene.position.y = 0.025/2;
+        gltf.scene.position.y = 0.025 / 2;
         this.scene.add(gltf.scene);
         this.loadingBar.visible = false;
+        gltf.scene.traverse((obj) => {
+          if (obj.isMesh) {
+            obj.castShadow = true;
+          }
+        });
         this.asset = gltf.scene;
       },
       (xhr) => {
-        this.loadingBar.progress = xhr.loaded/xhr.total;
+        this.loadingBar.progress = xhr.loaded / xhr.total;
       },
       (err) => {
         console.error(err);
