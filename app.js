6c941e85f6f46d91fa6c2306b9a61c38e58e3a22
johnsonsudre <johnsonsudre@gmail.com>
2022-11-04 10:09:10 -0300

    load gltf asset

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
-import * as THREE from "./three.module.js";
-import { OrbitControls } from "./OrbitControls.js";
+import * as THREE from "./modules/three.module.js";
+import { OrbitControls } from "./modules/OrbitControls.js";
+import { GLTFLoader } from "./modules/GLTFLoader.js";
 
 function newBox(params) {
-  const geometry = new THREE.BoxBufferGeometry();
-  const material = new THREE.MeshStandardMaterial({ color: 0x44ff44 });
-  return new THREE.Mesh(geometry, material);
+  const geometry = new THREE.BoxBufferGeometry(0.3, 0.005, 0.3);
+  const material = new THREE.MeshStandardMaterial({ color: 0xcccccc });
+  const mesh = new THREE.Mesh(geometry, material);
+  return mesh;
 }
 
-function getAspectRatio(){
-  return (window.innerWidth / window.innerHeight)
+function getAspectRatio() {
+  return window.innerWidth / window.innerHeight;
+}
+
+class OrbitController{
+  constructor(scope){
+    const control = new OrbitControls(scope.camera, scope.renderer.domElement);
+    control.enablePan = false;
+    control.target = new THREE.Vector3(0,0.1,0);
+    control.update();
+    return control;
+  }
+}
+
+class LoadingBar {
+  constructor() {}
 }
 
 class App {
@@ -16,14 +32,14 @@ class App {
     const container = document.createElement("div");
     document.body.appendChild(container);
 
-    this.camera = new THREE.PerspectiveCamera(60, getAspectRatio(), 0.1, 1000);
-    this.camera.position.set(0, 0, 5);
+    this.camera = new THREE.PerspectiveCamera(60, getAspectRatio(), 0.02, 10);
+    this.camera.position.set(0, 0.2, 0.4);
 
     this.scene = new THREE.Scene();
     this.scene.add(this.camera);
     this.scene.background = new THREE.Color(0xbbbbbb);
 
-    this.ambientLight = new THREE.HemisphereLight(0xddeeff, 0x333300, 1);
+    this.ambientLight = new THREE.HemisphereLight(0xddeeff, 0x333300, 2);
     this.scene.add(this.ambientLight);
 
     this.light = new THREE.DirectionalLight();
@@ -40,9 +56,25 @@ class App {
     this.box = newBox();
     this.scene.add(this.box);
 
-    new OrbitControls(this.camera, this.renderer.domElement);
+    this.loadingBar = new LoadingBar();
+    this.loadGLTF();
+
+    this.control = new OrbitController(this);
 
     window.addEventListener("resize", this.resize.bind(this));
+  }
+
+  loadGLTF() {
+    const loader = new GLTFLoader().setPath("./assets/");
+    loader.load(
+      "african_figurine_game_ready__2k_pbr.glb",
+      (gltf) => {
+        gltf.scene.position.y = 0.025/2;
+        this.box.add(gltf.scene);
+      },
+      (xhr) => {},
+      (err) => {}
+    );
   }
 
   resize() {
@@ -52,7 +84,7 @@ class App {
   }
 
   render() {
-    this.box.rotation.y += 0.0175;
+    this.box.rotation.y += 0.00175;
     this.renderer.render(this.scene, this.camera);
   }
 }
