0fefd4ef2515362d0d04208fcec4a499610b960b
johnsonsudre <johnsonsudre@gmail.com>
2022-11-04 07:43:15 -0300

    create html container

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 import * as THREE from "./three.module.js";
 import {OrbitControls} from "./OrbitControls.js";
+import { convertArray } from "three/src/animation/AnimationUtils.js";
 
 class App {
   constructor() {
-    
+    const container = document.createElement("div");
+    document.body.appendChild(container);
+    window.addEventListener("resize", this.resize.bind(this));    
   }
 
   resize() {}
