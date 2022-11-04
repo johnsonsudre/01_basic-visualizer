b1edb4814d9b9adbcd95d4438c76f23e4e7227f3
johnsonsudre <johnsonsudre@gmail.com>
2022-11-04 08:09:44 -0300

    setting basic scene: adding html container, camera, scene, renderer and loop

--- app.js
+++ app.js
@@ -1,17 +1,34 @@
 import * as THREE from "./three.module.js";
 import {OrbitControls} from "./OrbitControls.js";
-import { convertArray } from "three/src/animation/AnimationUtils.js";
 
 class App {
   constructor() {
     const container = document.createElement("div");
     document.body.appendChild(container);
-    window.addEventListener("resize", this.resize.bind(this));    
+    
+    const aspectRatio = window.innerWidth/window.innerHeight;
+    this.camera = new THREE.PerspectiveCamera(60, aspectRatio, 0.1, 1000);
+    this.camera.position.set(0,1.65,5);
+    
+    this.scene = new THREE.Scene();
+    this.scene.add(this.camera);
+    this.scene.background = new THREE.Color(0xBBBBBB);
+    
+    this.renderer = new THREE.WebGLRenderer({antialias: true});
+    this.renderer.setPixelRatio(window.devicePixelRatio);
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    container.appendChild(this.renderer.domElement);
+
+    this.renderer.setAnimationLoop(this.render.bind(this));
+
+    window.addEventListener("resize", this.resize.bind(this));
   }
 
   resize() {}
 
-  render() {}
+  render() {
+    this.renderer.render(this.scene, this.camera);
+  }
 }
 
 export { App };
